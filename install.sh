470819878e2d9d4ebc128848d211a9308067c9c7
Oskar Schöldström <public@oxy.fi>
2012-02-17 22:35:47 +0200

    Added check to see if git clone worked before running rsync, whops

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 git clone git://github.com/oxyc/dotfiles.git
-cd dotfiles
-rsync --exclude ".git/" --exclude "install.sh" --exclude "README.md" -av . ~
-source ~/.bash_profile
+if [ ! $? ]; then
+  cd dotfiles && rsync --exclude ".git/" --exclude "install.sh" --exclude "README.md" -av . ~
+  source ~/.bash_profile
+fi
