8a3ade99ee03f28a8e2ea2a8ba071b4ec476a47c
Oskar Schöldström <public@oxy.fi>
2013-02-09 00:43:55 +0200

    Tune xmonad

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,59 +1,44 @@
 import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Layout.Fullscreen
+import XMonad.Actions.SpawnOn
 import XMonad.Util.Run(spawnPipe)
+import XMonad.Layout.MouseResizableTile
+import XMonad.Layout.NoBorders
 
 myStartupHook = return ()
+-- myStartupHook = do
+--   spawnOn "1:web" "google-chrome"
+--   spawnOn "1:web" "urxvt -e 'mosh tlk'"
+--   spawnOn "2:term" "urxvt"
+--   spawnOn "3:mutt" "urxvt -e 'mutt'"
 
 myManageHook = composeAll
   [ resource  =? "desktop_window" --> doIgnore
   , className =? "VirtualBox"     --> doShift "4:vm"
+  , className =? "Thunar"         --> doFloat
+  , className =? "Qbittorrent"    --> doShift "9:torrent"
+  , className =? "Google-chrome"  --> doShift "1:web"
+  , title     =? "mutt"           --> doShift "3:mutt"
   ]
+
+myLayout = mouseResizableTile {
+    draggerType = BordersDragger -- Remove gap between tiles
+}
 
 main = do
   mobar <- spawnPipe "xmobar"
   xmonad =<< xmobar defaults
 
 defaults = defaultConfig {
-    terminal         = "urxvt"
-  -- , focusFollowMouse = myFocusFollowMouse
-  , modMask          = mod4Mask
-  , borderWidth      = 1
-  , workspaces       = ["1:term","2:web","3:code","4:vm","5:media", "6", "7", "8", "9"]
-  -- , layoutHook       = smartBorders
-  , manageHook       = myManageHook
-  , startupHook      = myStartupHook
+    terminal           = "urxvt"
+  , modMask            = mod4Mask
+  , borderWidth        = 1
+  , focusFollowsMouse  = True
+  , focusedBorderColor = "#ff0000"
+  , normalBorderColor  = "#000000"
+  , workspaces         = ["1:web","2:term","3:mutt","4:vm","5:media", "6", "7", "8", "9:torrent"]
+  , manageHook         = myManageHook
+  , startupHook        = myStartupHook
+  , layoutHook         = smartBorders $ myLayout
 }
-
--- myTerminal = "urxvt"
--- myModMask = mod4Mask
--- myBorderWidth = 1
--- myWorkspaces = 
--- 
--- myFocusFollowsMouse :: Bool
--- myFocusFollowsMouse = True
--- 
--- myStartupHook = return ()
--- 
--- myManageHook = composeAll
---   [ resource  =? "desktop_window" --> doIgnore
---   , className =? "VirtualBox"     --> doShift "4:vm"
---   , isFullscreen --> (doF W.focusDown <+> doFullFloat)]
--- 
--- main = do
---   xmproc <- spawnPipe "xmobar"
---   xmonad $ defaults {
---       manageHook = manageDocks <+> myManageHook
---   }
--- 
--- defaults = defaultConfig {
---     terminal         = myTerminal
---   , focusFollowMouse = myFocusFollowMouse
---   , modMask          = myModMask
---   , borderWidth      = myBorderWidth
---   , workspaces       = myWorkspaces
---   , layoutHook       = smartBorders
---   , manageHook       = myManageHook
---   , startupHook      = myStartupHook
--- }
--- 
