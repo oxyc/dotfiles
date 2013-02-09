afb1704616e43643505a42396050d28d2be6c8d1
Oskar Schöldström <public@oxy.fi>
2013-02-09 22:31:07 +0200

    Add grooveshark as a chrome app

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -20,13 +20,14 @@ myStartupHook = return ()
 --   spawnOn "3:mutt" "urxvt -e 'mutt'"
 
 myManageHook = manageDocks <+> composeAll
-  [ resource  =? "desktop_window" --> doIgnore
-  , className =? "VirtualBox"     --> doShift "4:vm"
-  , className =? "Thunar"         --> doFloat
-  , className =? "Qbittorrent"    --> doShift "9:torrent"
-  , className =? "Google-chrome"  --> doShift "1:web"
-  , title     =? "mutt"           --> doShift "3:mutt"
-  , isFullscreen                  --> (doF W.focusDown <+> doFullFloat)
+  [ resource  =? "desktop_window"       --> doIgnore
+  , resource  =? "www.grooveshark.com"  --> doShift "8:music"
+  , className =? "VirtualBox"           --> doShift "4:vm"
+  , className =? "Thunar"               --> doFloat
+  , className =? "Qbittorrent"          --> doShift "9:torrent"
+  , className =? "Google-chrome"        --> doShift "1:web"
+  , title     =? "mutt"                 --> doShift "3:mutt"
+  , isFullscreen                        --> (doF W.focusDown <+> doFullFloat)
   ]
 
 -- "avoidStruts" make space for the status bar
@@ -57,7 +58,7 @@ defaults = defaultConfig {
   , focusFollowsMouse  = True
   , focusedBorderColor = "#ff0000"
   , normalBorderColor  = "#000000"
-  , workspaces         = ["1:web","2:term","3:mutt","4:vm","5:media", "6", "7", "8", "9:torrent"]
+  , workspaces         = ["1:web","2:term","3:mutt","4:vm","5:media", "6", "7", "8:music", "9:torrent"]
   , manageHook         = myManageHook
   , startupHook        = myStartupHook
   , layoutHook         = myLayout
