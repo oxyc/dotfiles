fdf6e65d8191cd11631c77ba8064ab382fc46441
Oskar Schöldström <public@oxy.fi>
2013-02-28 09:38:20 +0200

    Update bash prompt

--- .bash/prompt.sh
+++ .bash/prompt.sh
@@ -1,30 +1,21 @@
 #!/bin/bash
-# git magic inspired by https://github.com/gf3/dotfiles/blob/master/.bash_prompt
-
-# Use status -uno to only traverse tracked files, alot faster.
-# @see https://github.com/mathiasbynens/dotfiles/issues/184
-parse_git_dirty() {
-  [[ $(git status -uno 2> /dev/null | tail -n1) != *"working directory clean"* ]] && echo "*"
-}
-
-parse_git_branch() {
-  git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/\1$(parse_git_dirty)/"
-}
-
-prompt_desc() {
-  OUTPUT=""
-  if [ -n "$SSH_CLIENT" ] ; then
-    OUTPUT="${OUTPUT}ssh/"
-  fi
-  if [[ -n $(git branch 2> /dev/null) ]] ; then
-    OUTPUT="${OUTPUT}$(parse_git_branch)/"
-  fi
-
-  [[ -n "$OUTPUT" ]] && echo " (${OUTPUT%?})"
-}
+# https://github.com/sindresorhus/pure/blob/master/prompt.zsh
+[ -z $DEFAULT_USERNAME ] && DEFAULT_USERNAME='oxy'
+[ -z $DEFAULT_HOSTNAME ] && DEFAULT_HOSTNAME='cindy'
 
 WHITE="\033[97m"
-YELLOW="\033[93m"
+GRAY="\033[90m"
 RESET="\033[0m"
 
-PS1="\n\[$WHITE\]\W\[$YELLOW\]\$(prompt_desc)\[$WHITE\]:\[$RESET\] "
+__prompt() {
+  local output=""
+  [ -n "$SSH_CLIENT" ] && local ssh="ssh/"
+  [ $USER != $DEFAULT_USERNAME ] && local user=" $USER@$HOSTNAME"
+  [ $HOSTNAME != $DEFAULT_HOSTNAME ] && local user=" $USER@$HOSTNAME"
+  local branch=$(git branch --no-color 2> /dev/null | sed -e '/^[^*]/d;s/* \(.*\)/\1/')
+  local dirty=$(git diff --quiet --ignore-submodules HEAD 2>/dev/null; [ $? -eq 1 ] && echo '*')
+
+  echo " ${ssh}${branch}${dirty}${user}"
+}
+
+PS1="\n\[$WHITE\]\W\[$GRAY\]\$(__prompt)\[$WHITE\]:\[$RESET\] "
