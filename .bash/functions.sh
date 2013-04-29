fc28ae0ccebde6f6d66519c968aa1c07a6a418a8
Oskar Schöldström <public@oxy.fi>
2013-04-29 22:58:38 +0300

    Add targz function

--- .bash/functions.sh
+++ .bash/functions.sh
@@ -249,3 +249,32 @@ bst() {
   $BROWSER "http://www.browserstack.com"
   java -jar ~/.local/lib/BrowserStackTunnel.jar $flags "$BROWSERSTACK_KEY" $args
 }
+
+# Create a .tar.gz archive, using `zopfli`, `pigz` or `gzip` for compression
+# https://github.com/mathiasbynens/dotfiles
+targz() {
+  local tmpFile="${@}.tar"
+  tar -cvf "${tmpFile}" --exclude=".DS_Store" "${@}" || return 1
+
+  size=$(
+    stat -f"%z" "${tmpFile}" 2> /dev/null; # OS X `stat`
+    stat --printf="%s" "${tmpFile}" 2> /dev/null # GNU `stat`
+  )
+
+  local cmd=""
+  if (( size < 52428800 )) && hash zopfli 2> /dev/null; then
+    # the .tar file is smaller than 50 MB and Zopfli is available; use it
+    cmd="zopfli"
+  else
+    if hash pigz 2> /dev/null; then
+      cmd="pigz"
+    else
+      cmd="gzip"
+    fi
+  fi
+
+  echo "Compressing .tar using \`${cmd}\`…"
+  "${cmd}" -v "${tmpFile}" || return 1
+  [ -f "${tmpFile}" ] && rm "${tmpFile}"
+  echo "${tmpFile}.gz created successfully."
+}
